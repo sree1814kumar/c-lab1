77344edd954e8c039d6c17c907dc91c578ada122
sree1814kumar <62131873+sree1814kumar@users.noreply.github.com>
2020-06-01 17:18:14 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+	{ 
+  		int a,b,c;         
+        	printf("\n Enter 3 numbers: ");
+          	scanf("%d%d%d",&a,&b,&c);	
+		if(a>b && a>c)
+			printf("a=%d is largest",a);		
+	 	else if(b>c && b>a)
+			printf("b=%d is largest",b);		
+	   	else
+			printf("c=%d is largest",c);
+        }
