9c3f2c91d4d8977ddd4e4f934df9bdf90d19e856
sree1814kumar <62131873+sree1814kumar@users.noreply.github.com>
2020-06-01 17:22:11 +0530

    Create string rev.c

---
+++ string rev.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse_str(char str[])
+	{ 
+		int i,n;
+  		char c;
+  		
+		n = strlen(str);
+  
+		for(i=0;i<n/2;i++)
+    			{   
+				c = str[i];
+         			str[i] = str[n-1-i];
+         			str[n-1-i] = c;
+     			}
+	}
+
+
+void main()
+	{
+  		char str[100];
+ 
+  		printf("\n Enter the string: "); 
+  		scanf("%s",str);
+  
+		reverse_str(str);
+  		
+		printf("\n Reversed string is: %s \n",str);
+}
