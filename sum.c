244feb130c6eb19c665ff032d4e6df3c2b79a813
sree1814kumar <62131873+sree1814kumar@users.noreply.github.com>
2020-06-01 17:22:33 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+	{ 
+		int num1,num2,sum;
+		printf("\n Enter the two numbers:\n");
+		scanf("%d%d", &num1, &num2);
+	  
+		sum = num1+num2;
+	  
+	  	printf("Sum = %d  \n", sum);
+        }
